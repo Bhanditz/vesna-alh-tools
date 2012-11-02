a3be8cd7bd5c9159676dbf6d6c14b60d1076727e
Zoltan Padrah <zoltan.padrah@ijs.si>
2012-11-02 14:56:54 +0100

    add dat2png an dat2ps tools
    these tools are useful for creating images from the .dat files

---
+++ dat2png.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# arg 1: filename to plot
+# arg 2: output filename
+gnuplot << EOF
+
+set terminal png size 1024,768
+set output '$2'
+
+set title '$1'
+set xlabel "f [MHz]"
+set ylabel "t [s]"
+set cblabel "P [dBm]"
+set grid
+unset key
+set pm3d map corners2color c1
+splot "$1" using (\$2/1e6):1:3
+
+EOF
+#
