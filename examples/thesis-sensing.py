bfafbe5a4765ae7a77edf40371da2ed27d51b68d
Zoltan Padrah <zoltan.padrah@ijs.si>
2012-11-04 18:39:15 +0100

    node 17 & 2

--- examples/thesis-sensing.py
+++ examples/thesis-sensing.py
@@ -32,7 +32,7 @@ def ism_24ghz(time_start_arg, nodef):
 	
 	# non-cognitive terminal's frequency
 	SignalGenerationRun(
-			alh = nodef(6),
+			alh = nodef(2),
 			time_start = time_start_arg + 25.0,
 			time_duration = 30,
 			device_id = 0,
@@ -41,7 +41,9 @@ def ism_24ghz(time_start_arg, nodef):
 			power = 0).program()
 
 	return MultiNodeSpectrumSensingRun(
-			nodes = [nodef(2), nodef(13), nodef(25), nodef(26) ],
+			nodes = [nodef(4), nodef(6), nodef(11),
+					nodef(13), nodef(24), nodef(25),
+					nodef(26) ],
 			time_start = time_start_arg,
 			time_duration = 60,
 			device_id = 0,
@@ -240,11 +242,11 @@ def main():
 
 	time_start = time.time() + extra_wait
 
-	# experiment = ism_24ghz(time_start, nodef)
+	experiment = ism_24ghz(time_start, nodef)
 	# experiment = test_n13(time_start, nodef)
 	# experiment = test_n11(time_start, nodef)
 	# experiment = test_n15(time_start, nodef)
-	experiment =  nodes_13_25(time_start, nodef)
+	# experiment =  nodes_13_25(time_start, nodef)
 
 	experiment.program()
 
